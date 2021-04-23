bf2876a3051e2f6ef943889e14f2c3efc601d9f4
Javier Segovia Córdoba <javier@segoviacordoba.com>
2021-04-23 12:18:56 +0200

    Add Version catalogs

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,16 @@
 rootProject.name = "hilt-install-binding"
 
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        google()
+        gradlePluginPortal()
+    }
+}
+
+enableFeaturePreview("VERSION_CATALOGS")
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
 include("compiler")
 include("annotations")
 include("sample")
